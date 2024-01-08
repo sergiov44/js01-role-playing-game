216e8f339c9927ce99eeb41188ad7f37ff322dd7
Sergio Vergara <sergiovergara123@gmail.com>
2024-01-08 11:53:49 -0500

    function goStore completed

--- script.js
+++ script.js
@@ -26,9 +26,13 @@ button3.onclick = fightDragon();
 // Initialized onClick() events
 
 function goStore() {
+    button1.innerText = "Buy 10 health (10 gold)";
+    button2.innerText = "Buy weapon (30 gold)";
+    button3.innerText = "Go to town square";
     button1.onclick = buyHealth;
     button2.onclick = buyWeapon;
     button3.onclick = goTown;
+    text.innerText = "You enter the store.";
 }
 
 function goCave() {
@@ -39,4 +43,6 @@ function fightDragon() {
     console.log("Fighting dragon.");
 }
 
-// Declared functions for buttons 1-2-3
+function buyHealth() {}
+function buyWeapon() {}
+function goTown() {}
