9386019e13f353801ec8bdea7439f6fa7cbe21b2
Sergio Vergara <sergiovergara123@gmail.com>
2024-01-10 17:36:23 -0500

    corrected syntax and completed buyWeapon function

--- script.js
+++ script.js
@@ -115,6 +115,10 @@ function buyWeapon() {
         } else {
             text.innerText = "You do not have enough gold to buy a weapon.";
         }
+    } else {
+        text.innerText = "You already have the most powerful weapon!";
+        button2.innerText = "Sell weapon for 15 gold";
+        button2.onclick = sellWeapon;
     }
 }
 
