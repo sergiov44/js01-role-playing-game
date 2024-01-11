17bd19b12a78945b38ea30ca89f9f10c3c380133
Sergio Vergara <sergiovergara123@gmail.com>
2024-01-11 01:39:11 -0500

    completed lose and restart functions, and added lose object inside locations array

--- script.js
+++ script.js
@@ -84,6 +84,12 @@ const locations = [
         "button text": ["Go to town square", "Go to town square", "Go to town square"],
         "button functions": [goTown, goTown, goTown],
         text: 'The monster screams "Arg!" as it dies. You gain experience points and find gold.'
+    },
+    {
+        name: "lose",
+        "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
+        "button functions": [restart, restart, restart],
+        text: "You die. ☠️"
     }
 ];
 
@@ -210,4 +216,18 @@ function defeatMonster() {
     update(locations[4]);
 }
 
-function lose() {}
+function lose() {
+    update(locations[5]);
+}
+
+function restart() {
+    xp = 0;
+    health = 100;
+    gold = 50;
+    currentWeapon = 0;
+    inventory = ["stick"];
+    goldText.innerText = gold;
+    healthText.innerText = health;
+    xpText.innerText = xp;
+    goTown();
+}
